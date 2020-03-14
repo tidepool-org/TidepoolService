2b8057dd619be70080abd9ca5cb6644f50d74a85
Darin Krauss <darinkrauss@gmail.com>
2020-03-13 19:13:45 -0700

    Initial integration with TidepoolKit
    - Include Carthage build to lessen impact to build time (like DashKit)

--- TidepoolServiceKit/TidepoolService.swift
+++ TidepoolServiceKit/TidepoolService.swift
@@ -7,6 +7,12 @@
 //
 
 import LoopKit
+import TidepoolKit
+
+public protocol SessionStorage {
+    func setSession(_ session: TSession?, for service: String) throws
+    func getSession(for service: String) throws -> TSession?
+}
 
 public final class TidepoolService: Service {
 
@@ -16,20 +22,89 @@ public final class TidepoolService: Service {
 
     public weak var serviceDelegate: ServiceDelegate?
 
-    public init() {}
+    public lazy var sessionStorage: SessionStorage? = KeychainManager()
 
-    public init?(rawState: RawStateValue) {}
+    public let tapi = TAPI()
 
-    public var rawState: RawStateValue {
-        return [:]
+    public private (set) var error: Error?
+
+    private let id: String
+
+    private var session: TSession? {
+        didSet {
+            saveSession()
+        }
     }
 
-    public func completeCreate() {}
+    public init() {
+        self.id = UUID().uuidString
+    }
+
+    public init?(rawState: RawStateValue) {
+        guard let id = rawState["id"] as? String else {
+            return nil
+        }
+        self.id = id
+        restoreSession()
+    }
+
+    public var rawState: RawStateValue {
+        var rawValue: RawStateValue = [:]
+        rawValue["id"] = id
+        return rawValue
+    }
+
+    public func completeCreate(withSession session: TSession) {
+        self.session = session
+    }
 
     public func completeUpdate() {
         serviceDelegate?.serviceDidUpdateState(self)
     }
 
-    public func completeDelete() {}
+    public func completeDelete() {
+        self.session = nil
+    }
 
+    private var sessionService: String { "org.tidepool.TidepoolService.\(id)" }
+
+    private func saveSession() {
+        do {
+            try sessionStorage?.setSession(session, for: sessionService)
+        } catch let error {
+            self.error = error
+        }
+    }
+
+    private func restoreSession() {
+        do {
+            self.session = try sessionStorage?.getSession(for: sessionService)
+        } catch let error {
+            self.error = error
+        }
+    }
+}
+
+extension KeychainManager: SessionStorage {
+    public func setSession(_ session: TSession?, for service: String) throws {
+        try replaceGenericPassword(nil, forService: service)
+        guard let session = session else {
+            return
+        }
+        let sessionData = try JSONEncoder.tidepool.encode(session)
+        guard let sessionString = String(data: sessionData, encoding: .utf8) else {
+            throw SessionStorageError()
+        }
+        try replaceGenericPassword(sessionString, forService: service)
+    }
+
+    public func getSession(for service: String) throws -> TSession? {
+        let sessionString = try getGenericPasswordForService(service)
+        guard let sessionData = sessionString.data(using: .utf8) else {
+            throw SessionStorageError()
+        }
+        return try JSONDecoder.tidepool.decode(TSession.self, from: sessionData)
+    }
+
+    public struct SessionStorageError: Error {}
 }
