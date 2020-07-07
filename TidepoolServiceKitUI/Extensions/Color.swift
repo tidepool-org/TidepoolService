3d2c379ccdd2fa953319c5881e50abcd7dced726
Anna <anna@tidepool.org>
2020-07-07 15:18:55 -0700

    Use secondary color if in dark mode, else blue gray

---
+++ TidepoolServiceKitUI/Extensions/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  TidepoolServiceKitUI
+//
+//  Created by Anna Quinlan on 7/7/20.
+//  Copyright © 2020 LoopKit Authors. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    public static let blueGray = Color(.tidepoolGray)
+}
