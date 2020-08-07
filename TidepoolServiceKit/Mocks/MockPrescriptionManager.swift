26b3b9aaffffe9e013f7d7186b30b165c130e805
Anna Quinlan <31571514+novalegra@users.noreply.github.com>
2020-08-06 19:35:46 -0700

    Update mock prescription values per discussion with QA (#39)

--- TidepoolServiceKit/Mocks/MockPrescriptionManager.swift
+++ TidepoolServiceKit/Mocks/MockPrescriptionManager.swift
@@ -20,55 +20,35 @@ public class MockPrescriptionManager {
             let glucoseTargetRangeSchedule =  GlucoseRangeSchedule(
                 rangeSchedule: DailyQuantitySchedule(unit: .milligramsPerDeciliter,
                     dailyItems: [RepeatingScheduleValue(startTime: .hours(0), value: DoubleRange(minValue: 100.0, maxValue: 110.0)),
-                                 RepeatingScheduleValue(startTime: .hours(8), value: DoubleRange(minValue: 95.0, maxValue: 105.0)),
-                                 RepeatingScheduleValue(startTime: .hours(14), value: DoubleRange(minValue: 95.0, maxValue: 105.0)),
-                                 RepeatingScheduleValue(startTime: .hours(16), value: DoubleRange(minValue: 100.0, maxValue: 110.0)),
-                                 RepeatingScheduleValue(startTime: .hours(18), value: DoubleRange(minValue: 90.0, maxValue: 100.0)),
-                                 RepeatingScheduleValue(startTime: .hours(21), value: DoubleRange(minValue: 110.0, maxValue: 120.0))],
+                                 RepeatingScheduleValue(startTime: .hours(8), value: DoubleRange(minValue: 90.0, maxValue: 100.0)),
+                                 RepeatingScheduleValue(startTime: .hours(21), value: DoubleRange(minValue: 100.0, maxValue: 110.0))],
                     timeZone: timeZone)!,
                 override: GlucoseRangeSchedule.Override(value: DoubleRange(minValue: 80.0, maxValue: 90.0),
                                                         start: Date().addingTimeInterval(.minutes(-30)),
                                                         end: Date().addingTimeInterval(.minutes(30)))
             )
             let basalRateSchedule = BasalRateSchedule(
-                dailyItems: [RepeatingScheduleValue(startTime: .hours(0), value: 1.0),
-                             RepeatingScheduleValue(startTime: .hours(8), value: 1.125),
-                             RepeatingScheduleValue(startTime: .hours(10), value: 1.25),
-                             RepeatingScheduleValue(startTime: .hours(12), value: 1.5),
-                             RepeatingScheduleValue(startTime: .hours(14), value: 1.25),
-                             RepeatingScheduleValue(startTime: .hours(16), value: 1.5),
-                             RepeatingScheduleValue(startTime: .hours(18), value: 1.25),
-                             RepeatingScheduleValue(startTime: .hours(21), value: 1.0)],
+                dailyItems: [RepeatingScheduleValue(startTime: .hours(0), value: 1),
+                             RepeatingScheduleValue(startTime: .hours(15), value: 0.85)],
                 timeZone: timeZone)!
             let insulinSensitivitySchedule = InsulinSensitivitySchedule(
                 unit: .milligramsPerDeciliter,
                 dailyItems: [RepeatingScheduleValue(startTime: .hours(0), value: 45.0),
-                             RepeatingScheduleValue(startTime: .hours(8), value: 40.0),
-                             RepeatingScheduleValue(startTime: .hours(10), value: 35.0),
-                             RepeatingScheduleValue(startTime: .hours(12), value: 30.0),
-                             RepeatingScheduleValue(startTime: .hours(14), value: 35.0),
-                             RepeatingScheduleValue(startTime: .hours(16), value: 40.0)],
+                             RepeatingScheduleValue(startTime: .hours(9), value: 55.0)],
                 timeZone: timeZone)!
             let carbRatioSchedule = CarbRatioSchedule(
                 unit: .gram(),
                                                       
-                dailyItems: [RepeatingScheduleValue(startTime: .hours(0), value: 10.0),
-                             RepeatingScheduleValue(startTime: .hours(8), value: 12.0),
-                             RepeatingScheduleValue(startTime: .hours(10), value: 9.0),
-                             RepeatingScheduleValue(startTime: .hours(12), value: 10.0),
-                             RepeatingScheduleValue(startTime: .hours(14), value: 11.0),
-                             RepeatingScheduleValue(startTime: .hours(16), value: 12.0),
-                             RepeatingScheduleValue(startTime: .hours(18), value: 8.0),
-                             RepeatingScheduleValue(startTime: .hours(21), value: 10.0)],
+                dailyItems: [RepeatingScheduleValue(startTime: .hours(0), value: 10.0)],
                 timeZone: timeZone)!
             
             let therapySettings = TherapySettings(
                 glucoseTargetRangeSchedule: glucoseTargetRangeSchedule,
                 preMealTargetRange: DoubleRange(minValue: 80.0, maxValue: 90.0),
-                workoutTargetRange: DoubleRange(minValue: 130.0, maxValue: 140.0),
-                maximumBasalRatePerHour: 3,
-                maximumBolus: 5,
-                suspendThreshold: GlucoseThreshold(unit: .milligramsPerDeciliter, value: 80),
+                workoutTargetRange: DoubleRange(minValue: 140.0, maxValue: 160.0),
+                maximumBasalRatePerHour: 5,
+                maximumBolus: 10,
+                suspendThreshold: GlucoseThreshold(unit: .milligramsPerDeciliter, value: 75),
                 insulinSensitivitySchedule: insulinSensitivitySchedule,
                 carbRatioSchedule: carbRatioSchedule,
                 basalRateSchedule: basalRateSchedule,
